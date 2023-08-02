fa7bb431ccdd3ce6ebf0be43848e78df58b40f8e
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-02 19:11:16 +0200

    fiddle

--- backend/config/database.go
+++ backend/config/database.go
@@ -1,26 +1,40 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"github.com/rs/zerolog/log"
+	"fmt"
+	"go_rest/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"os"
+)
+
+//	func DatabaseConnection() *gorm.DB {
+//		err := godotenv.Load()
+//		if err != nil {
+//			log.Error().Err(err).Msg("error loading .env file")
+//		}
+//		dsn := os.Getenv("DATABASE_URL")
+//		if dsn == "" {
+//			log.Error().Err(err).Msg("DATABASE_URL environment variable is not set")
+//		}
+//		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//		if err != nil {
+//			log.Error().Err(err).Msg("There was an error connecting to the database")
+//		}
+//		log.Info().Msg("Database connection established")
+//		return db
+//	}
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "kris"
+	password = "post123"
+	dbName   = "go_test"
 )
 
 func DatabaseConnection() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Error().Err(err).Msg("error loading .env file")
-	}
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		log.Error().Err(err).Msg("DATABASE_URL environment variable is not set")
-	}
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Error().Err(err).Msg("There was an error connecting to the database")
-	}
-	log.Info().Msg("Database connection established")
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+	db, err := gorm.Open(postgres.Open(sqlInfo), &gorm.Config{})
+	utils.ErrorPanic(err)
+
 	return db
 }
--- backend/data/request/create_user_request.go
+++ backend/data/request/create_user_request.go
@@ -2,6 +2,6 @@ package request
 
 type CreateUserRequest struct {
 	Name        string `validate:"required,min=1,max=200" json:"name"`
-	DateOfBirth string `validate:"required" json:"dob"`
+	DateOfBirth string `validate:"required" json:"date_of_birth"`
 	Email       string `validate:"required,email" json:"email"`
 }

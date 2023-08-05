9f6d3a20ce35548e550857d0230f0e25c033b89f
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-05 10:58:34 +0200

    added custom packages for cli tooling and config parsing. Introduced custom method to marhsall the UserResponseJson date time to date format alone

--- backend/data/response/user_response.go
+++ backend/data/response/user_response.go
@@ -1,10 +1,21 @@
 package response
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 type UserResponse struct {
 	Id          int       `json:"id"`
 	Name        string    `json:"name"`
-	DateOfBirth time.Time `json:"date_of_birth"`
+	DateOfBirth time.Time `json:"_"`
 	Email       string    `json:"email"`
+}
+
+func (ur *UserResponse) MarshalJSON() ([]byte, error) {
+	type Alias UserResponse
+	return []byte(`{"id":` + strconv.Itoa(ur.Id) +
+		`,"name":"` + ur.Name +
+		`","date_of_birth":"` + ur.DateOfBirth.Format("2006-01-02") + // Format the date
+		`","email":"` + ur.Email + `"}`), nil
 }

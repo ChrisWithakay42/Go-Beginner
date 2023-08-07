c5cb765162ad6328a71c033289b2bfe83b268b5c
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-07 20:06:18 +0200

    generic repository

--- backend/repository/user_repository.go
+++ backend/repository/user_repository.go
@@ -11,3 +11,11 @@ type UserRepository interface {
 	FindById(usersId int) (users model.User, err error)
 	FindAll() []model.User
 }
+
+type Repository interface {
+	Save(data interface{})
+	Update(data interface{})
+	Delete(id int)
+	FindById(id int) (interface{}, error)
+	FindAll() []interface{}
+}
